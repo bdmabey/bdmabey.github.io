f34a6d200e778c79bbba010431cc4435ae9061d4
Braden Mabey <braden.mabey@gmail.com>
2021-03-14 22:54:04 -0400

    Added some ui upgrades
    diplayData function to simplify the update of data. refactored to increase understandability.

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,50 @@
 var goldIdle = {
     gold : 0,
     clickIncrease : 1,
-    clickUpgrades : 1,
-    mineCost : 100,
-    mineLevel : 0
+    clickers : 1,
+    mineCost : 110,
+    mineLevel : 0,
+    goldPerSecond : 0
 };
 
+var goldPerSecond = goldIdle.goldPerSecond
 var gold = goldIdle.gold
 var clickIncrease = goldIdle.clickIncrease
-var clickUpgrades = goldIdle.clickUpgrades
-var clickCost = Math.floor(10 * Math.pow(1.1, clickUpgrades) + 20)
+var clickers = goldIdle.clickers
+var clickCost = Math.floor(10 * Math.pow(1.1, clickers) + 20)
 var mineCost = goldIdle.mineCost
 var mineLevel = goldIdle.mineLevel
 
+displayData("buyClicker", clickCost, null, null);
+displayData("goldMines", mineLevel, null, null)
+displayData("mineCost", mineCost, null, null)
+displayData("goldPerSecond", goldPerSecond, null, null)
+displayData("clickers", clickers, null, null)
 
-document.getElementById("upgradeGoldCost").innerHTML = clickCost
-document.getElementById("goldMines").innerHTML = mineLevel
-document.getElementById("mineCost").innerHTML = mineCost
+function displayData(id, value, preText, postText) {
+    document.getElementById(id).innerHTML = preText + value + postText;
+}
 
 function clickUpgrade() {
-    clickCost = Math.floor(10 * Math.pow(1.1, clickUpgrades) + 20)
+    clickCost = Math.floor(10 * Math.pow(1.1, clickers) + 20)
     if(gold >= clickCost) {
         clickIncrease += 1
-        clickUpgrades = clickUpgrades + 1;
+        clickers = clickers + 1;
         gold = gold - clickCost;
-        document.getElementById("gold").innerHTML = gold + " Gold"
-        document.getElementById("goldClick").innerHTML = "Gain " + clickIncrease + " Gold";
+        displayData("gold", gold, null, " Gold")
+        displayData("goldClick", clickIncrease, "Gain ", " Gold")
+        displayData("clickers", clickers, null, null)
     }
     else {
         alert("Not Enough Gold!")
     }
-    var nextCost = Math.floor(10 * Math.pow(1.1, clickUpgrades) + 20);
-    document.getElementById("upgradeGoldCost").innerHTML = nextCost;
+    var nextCost = Math.floor(10 * Math.pow(1.1, clickers) + 20);
+    displayData("buyClicker", nextCost, null, null)
 }
 
 function goldClick() {
     gold = gold + clickIncrease;
-    document.getElementById("gold").innerHTML = gold + " Gold";
+    displayData("gold", gold, null, " Gold")
 }
 
 function buyMine() {
@@ -44,19 +52,21 @@ function buyMine() {
     if(gold >= mineCost) {
         mineLevel += 1;
         gold = gold - mineCost;
-        document.getElementById("goldMines").innerHTML = mineLevel
-        document.getElementById("gold").innerHTML = gold + " Gold"
+        goldPerSecond = goldPerSecond + 1
+        displayData("goldMines", mineLevel, null, null)
+        displayData("gold", gold, null, " Gold")
+        displayData("goldPerSecond", goldPerSecond, null, null)
     }
     else {
         alert("Not Enough Gold!");
     }
     mineCost = Math.floor(10 * Math.pow(1.3, mineLevel) + 100)
-    document.getElementById("mineCost").innerHTML = mineCost
+    displayData("mineCost", mineCost, null, null)
 }
 
 function idleGold() {
     gold = gold + mineLevel;
-    document.getElementById("gold").innerHTML = gold + " Gold"
+    displayData("gold", gold, null, " Gold")
 }
 
 window.setInterval(function() {
